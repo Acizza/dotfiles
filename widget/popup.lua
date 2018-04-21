10c9d827aa721e08905fc7b41e127e1649d1c4be
Acizza <jonathanmce@gmail.com>
2018-04-21 03:25:38 -0700

    Create basic interface for a widget popup

---
+++ widget/popup.lua
@@ -0,0 +1,33 @@
+local awful = require("awful")
+local config = require("config")
+local gears = require("gears")
+
+WidgetPopup = {}
+WidgetPopup.__index = WidgetPopup
+
+function WidgetPopup:new(options)
+    local wibar = awful.wibar {
+        width = options.width,
+        height = options.height,
+        opacity = config.panel_opacity,
+        visible = false,
+    }
+
+    local widget_popup = {
+        wibar = wibar,
+    }
+
+    setmetatable(widget_popup, WidgetPopup)
+    return widget_popup
+end
+
+function WidgetPopup:setup(...)
+    self.wibar:setup(...)
+end
+
+function WidgetPopup:toggle()
+    awful.placement.under_mouse(self.wibar)
+    awful.placement.no_offscreen(self.wibar)
+
+    self.wibar.visible = not self.wibar.visible
+end
