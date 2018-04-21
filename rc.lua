f5b1a7b5a8df0e106365b70f99fc65aa5185f25d
Acizza <jonathanmce@gmail.com>
2018-04-21 01:39:33 -0700

    Move global / client key bindings to key_bindings.lua

--- rc.lua
+++ rc.lua
@@ -12,6 +12,7 @@ local config = require("config")
 -- Init the theme now so widgets can access custom theme values
 beautiful.init(config.theme_path)
 
+local key_bindings = require("key_bindings")
 local util = require("util")
 
 local run_dialog = require("run_dialog")
@@ -156,169 +157,53 @@ do
     end)
 end
 
-do
-    local global_keys = gears.table.join(
-        -- Open the shutdown menu
-        awful.key({ config.modkey }, "End", shutdown_menu.open),
-        -- Shift focus to the right
-        awful.key({ config.modkey }, "Right", function() awful.client.focus.byidx(1) end),
-        -- Shift focus to left
-        awful.key({ config.modkey }, "Left", function() awful.client.focus.byidx(-1) end),
-        -- Shift client to the right
-        awful.key({ config.modkey, "Shift" }, "Right", function() awful.client.swap.byidx(1) end),
-        -- Shift client to the left
-        awful.key({ config.modkey, "Shift" }, "Left", function() awful.client.swap.byidx(-1) end),
-        -- Increase the number of tag columns
-        awful.key({ config.modkey, "Control", "Shift" }, "Right", function() awful.tag.incncol(1, nil, true) end),
-        -- Decrease the number of tag columns
-        awful.key({ config.modkey, "Control", "Shift" }, "Left", function() awful.tag.incncol(-1, nil, true) end),
-        -- Increase master client width factor
-        awful.key({ config.modkey, "Control" }, "Right", function() awful.tag.incmwfact(0.05) end),
-        -- Decrease master client width factor
-        awful.key({ config.modkey, "Control" }, "Left", function() awful.tag.incmwfact(-0.05) end),
-        -- Increase client height factor
-        awful.key({ config.modkey, "Control" }, "Down", function() awful.client.incwfact(0.05) end),
-        -- Decrease client height factor
-        awful.key({ config.modkey, "Control" }, "Up", function() awful.client.incwfact(-0.05) end),
-        -- Restart Awesome
-        awful.key({ config.modkey, "Control", "Shift" }, "r", awesome.restart),
-        -- Go to next window layout
-        awful.key({ config.modkey }, "space", function() awful.layout.inc(1) end),
-        -- Go to previous window layout
-        awful.key({ config.modkey, "Shift" }, "space", function() awful.layout.inc(-1) end),
-        -- Open run dialog
-        awful.key({ config.modkey }, "d", run_dialog.open),
-        -- Raise volume
-        awful.key({}, "XF86AudioRaiseVolume", volume_module.increment_source),
-        -- Lower volume
-        awful.key({}, "XF86AudioLowerVolume", volume_module.decrement_source),
-        -- Toggle volume mute
-        awful.key({}, "XF86AudioMute", volume_module.toggle_source_mute)
-    )
+root.keys(key_bindings.global)
 
-    for i,shortcut in pairs(config.program_shortcuts) do
-        global_keys = gears.table.join(global_keys,
-            awful.key(shortcut.modifier, shortcut.key, function()
-                awesome.spawn(shortcut.exec)
-            end)
-        )
-    end
-
-    -- Add keys for each tag
-    for i = 1, #config.tags do
-        global_keys = gears.table.join(global_keys,
-            -- View tag
-            awful.key({ config.modkey }, "#" .. 9 + i, function()
-                local screen = awful.screen.focused()
-                local tag = screen.tags[i]
-
-                if tag then
-                    tag:view_only()
-                end
-            end),
-            -- Toggle tag display
-            awful.key({ config.modkey, "Control" }, "#" .. 9 + i, function()
-                local screen = awful.screen.focused()
-                local tag = screen.tags[i]
-
-                if tag then
-                    awful.tag.viewtoggle(tag)
-                end
-            end),
-            -- Move client to tag
-            awful.key({ config.modkey, "Shift" }, "#" .. 9 + i, function()
-                if client.focus then
-                    local tag = client.focus.screen.tags[i]
-
-                    if tag then
-                        client.focus:move_to_tag(tag)
-                    end
-                end
-            end),
-            -- Toggle tag on focused client
-            awful.key({ config.modkey, "Control", "Shift" }, "#" .. 9 + i, function()
-                if client.focus then
-                    local tag = client.focus.screen.tags[i]
-
-                    if tag then
-                        client.focus:toggle_tag(tag)
-                    end
-                end
-            end)
-        )
-    end
-
-    root.keys(global_keys)
-
-    local client_keys = gears.table.join(
-        -- Kill focused process
-        awful.key({ config.modkey, "Shift" }, "q", function(c) c:kill() end),
-        -- Toggle window fullscreen
-        awful.key({ config.modkey }, "f", function(c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end),
-        -- Toggle floating
-        awful.key({ config.modkey, "Control" }, "space",  awful.client.floating.toggle),
-        -- Toggle keep on top
-        awful.key({ config.modkey }, "t", function(c) c.ontop = not c.ontop end)
-    )
-
-    local client_buttons = gears.table.join(
-        awful.button({}, 1, function(c)
-            client.focus = c
-            c:raise()
-        end),
-        awful.button({ config.modkey }, 1, awful.mouse.client.move),
-        awful.button({ config.modkey }, 3, awful.mouse.client.resize)
-    )
-
-    awful.rules.rules = {
-        { -- Rules for all clients
-            rule = { },
-            properties = {
-                border_width = beautiful.border_width,
-                border_color = beautiful.border_normal,
-                focus = awful.client.focus.filter,
-                raise = true,
-                keys = client_keys,
-                buttons = client_buttons,
-                screen = awful.screen.preferred,
-                placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+awful.rules.rules = {
+    { -- Rules for all clients
+        rule = { },
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = key_bindings.client.keys,
+            buttons = key_bindings.client.buttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+        }
+    },
+    { -- Floating clients.
+        rule_any = gears.table.join(config.floating_windows, {
+            role = {
+                "pop-up",
             }
+        }),
+        properties = {
+            floating = true
+        }
+    },
+    { -- Assign Steam to its own tag, and remove its border
+        rule = {
+            instance = "Steam"
         },
-        { -- Floating clients.
-            rule_any = gears.table.join(config.floating_windows, {
-                role = {
-                    "pop-up",
-                }
-            }),
-            properties = {
-                floating = true
-            }
+        properties = {
+            tag = "steam",
+            border_width = 0,
+        }
+    },
+    { -- Add titlebars to normal clients and dialogs
+        rule_any = {
+            type = { "normal", "dialog" }
         },
-        { -- Assign Steam to its own tag, and remove its border
-            rule = {
-                instance = "Steam"
-            },
-            properties = {
-                tag = "steam",
-                border_width = 0,
-            }
+        except = {
+            instance = "Steam"
         },
-        { -- Add titlebars to normal clients and dialogs
-            rule_any = {
-                type = { "normal", "dialog" }
-            },
-            except = {
-                instance = "Steam"
-            },
-            properties = {
-                titlebars_enabled = true
-            },
+        properties = {
+            titlebars_enabled = true
         },
-    }
-end
+    },
+}
 
 client.connect_signal("manage", function(c)
     if awesome.startup and
