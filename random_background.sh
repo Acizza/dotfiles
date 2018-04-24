e23b1eb095bb3a3394da3dda874b03d62e17ee86
Acizza <jonathanmce@gmail.com>
2018-04-24 03:51:59 -0700

    Remove i3 related files

--- random_background.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-backgrounds=(~/backgrounds/active/*.{jpg,png})
-
-rand=$[ $RANDOM % ${#backgrounds[@]} ]
-background=${backgrounds[$rand]}
-
-feh --bg-scale $background
-wal -q -n -i $background
