458231592f29f057957ed6b05f39353be59a9735
Acizza <jonathanmce@gmail.com>
2018-04-28 07:12:32 -0700

    Add spotify.get_metadata function

--- module/spotify.lua
+++ module/spotify.lua
@@ -3,6 +3,26 @@ local spotify = {}
 local awful = require("awful")
 local key_bindings = require("key_bindings")
 
+local string_match = string.match
+
+function spotify.get_metadata(callback)
+    awful.spawn.easy_async("sp metadata", function(stdout, _, _, error_code)
+        if error_code ~= 0 then
+            callback(nil)
+            return
+        end
+
+        local metadata = {
+            title = string_match(stdout, "title|(.-)\n"),
+            artist = string_match(stdout, "artist|(.-)\n"),
+            album = string_match(stdout, "album|(.-)\n"),
+            cover_url = string_match(stdout, "artUrl|(.-)\n"),
+        }
+
+        callback(metadata)
+    end)
+end
+
 function spotify.prev_track()
     awesome.spawn("sp prev")
 end
