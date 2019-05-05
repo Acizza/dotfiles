db169702161cfde6eaada85d616fd39b2590fcf8
Acizza <jgit@tuta.io>
2019-05-04 18:32:38 -0700

    [config] Remove ue4_fix hook from wpfxm
    Now that Wine has support for FAudio, we no longer need this workaround.

--- .config/wpfxm/hooks/ue4_fix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Needed for XAudio libraries (xact doesn't provide the correct ones)
-winetricks directx9
-
-# Despite tricking UE4 to use an older version of this library, it seems to work perfectly
-ln -s "$WINEPREFIX"/drive_c/windows/system32/XAPOFX1_4.dll "$WINEPREFIX"/drive_c/windows/system32/XAPOFX1_5.dll
