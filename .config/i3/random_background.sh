be1384ee89e9e8efe453513e238408b508546687
Acizza <jgit@tuta.io>
2019-12-23 14:57:46 -0800

    remove i3 config

--- .config/i3/random_background.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-FILES=(~/backgrounds/active/*.{png,jpg})
-IDX=$(( $RANDOM % ${#FILES[@]} ))
-
-wal -q -i "${FILES[$IDX]}"
-feh --bg-scale ${FILES[$IDX]}
--- .config/polybar/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q .polybar-wrappe
-polybar main
