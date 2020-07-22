cec4faeb527788ed97abe9f12e1e3b4f1073ab09
Acizza <jgit@tuta.io>
2020-07-22 14:42:07 -0700

    wpfxm: Remove unnecessary hooks

--- .config/wpfxm/hooks/setup_d9vk.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-setup_d9vk uninstall --symlink
-setup_d9vk install --symlink
--- .config/wpfxm/hooks/setup_dxvk.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-setup_dxvk uninstall --symlink
-setup_dxvk install --symlink
